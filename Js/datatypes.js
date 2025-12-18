9f2329bd8a1d215c0b4d6ebd230c387e9c2f2e0c
learnify <mdredwanhossain2007@gmail.com>
2025-12-18 23:54:35 +0600

    Add file

---
+++ Js/datatypes.js
@@ -0,0 +1,26 @@
+// Primitive Data Types 
+
+  // let age = 24;
+
+  // let price = 159.90;
+
+  // let fullName = "Tony Stark";
+
+  // isFollow = True;
+
+  // let x = null;
+
+  // let y = BigInt("123");
+
+  // let z = Symbol("Hello JavaScript!");
+
+  // Non-Premitive Data types 
+
+  const student = {
+    fullName: "Redwan Hossain",
+    age: "18",
+    gpa: "7.69",
+    isPass: true,
+  };
+  student["age"] = student["age"] + 1;
+  console.log(student.age);
