4f28dd56879f66257247b2d6cc2631178526f13f
learnify <mdredwanhossain2007@gmail.com>
2025-12-19 13:12:25 +0600

    Update file

--- Chapter2/js/Comments.js
+++ Chapter2/js/Comments.js
@@ -1,2 +1,3 @@
 /* This is a code that prints Hello world! on our console window*/
+//Learning JavaScript First Time
 console.log("Hello World!");
---
+++ Chapter2/js/operators.js
@@ -0,0 +1,14 @@
+// Arithmatic Operators
+let a = 10;
+let b = 5;
+let c = a + b;
+
+console.log("a =", 10, "& b =", 5);
+console.log("a + b =", a + b); //Addition 
+console.log("a - b =", a - b); //Substraction
+console.log("a * b =", a * b); //Multiplication
+console.log("a / b =", a / b); //Division
+console.log("a % b =", a % b); //Modulus
+console.log("a ** b =", a ** b); //Exponentiation 10^5=100000
+
+// console.log("a + b =",c);
