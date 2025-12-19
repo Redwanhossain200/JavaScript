8a219ff30ada95aac576f51532d59fba75e0a2aa
learnify <mdredwanhossain2007@gmail.com>
2025-12-19 11:37:11 +0600

    Update file

--- Js/datatypes.js
+++ Js/datatypes.js
@@ -1,26 +1,43 @@
 // Primitive Data Types 
 
-  // let age = 24;
-
-  // let price = 159.90;
-
-  // let fullName = "Tony Stark";
-
-  // isFollow = True;
-
-  // let x = null;
-
-  // let y = BigInt("123");
-
-  // let z = Symbol("Hello JavaScript!");
-
-  // Non-Premitive Data types 
+  let age = 24;
+console.log(age);
+  
+  let price = 159.90;
+console.log(price);
+ 
+let fullname = "Tony Stark";
+console.log(fullname);
+  
+isFollow = True;
+console.log(isfolllow);
+  
+let x = null;
+console.log(x);
+  
+let y = BigInt("123");
+console.log(y);
+  
+let z = Symbol("Hello JavaScript!");
+console.log(z);
+  
+  // Non-Premitive Data types
 
   const student = {
     fullName: "Redwan Hossain",
-    age: "18",
-    gpa: "7.69",
+    age: 18,
+    gpa: 7.69,
     isPass: true,
   };
+  
+   console.log(student);
+
+  student["name"] = "Rafsan Ahmed";
+   console.log(student["name"]);
+
   student["age"] = student["age"] + 1;
-  console.log(student["age"]);
+   console.log(student["age"]);
+
+  student["gpa"] = student["gpa"] + 1;
+   console.log(student["gpa"]);
+ 
