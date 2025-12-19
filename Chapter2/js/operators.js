00e5628710713b5db83729e4ffe4d2a9ebecfd0d
learnify <mdredwanhossain2007@gmail.com>
2025-12-19 13:23:57 +0600

    Update file

--- Chapter2/js/operators.js
+++ Chapter2/js/operators.js
@@ -2,6 +2,7 @@
 let a = 10;
 let b = 5;
 let c = a + b;
+// console.log("a + b =",c);
 
 console.log("a =", 10, "& b =", 5);
 console.log("a + b =", a + b); //Addition 
@@ -10,5 +11,3 @@ console.log("a * b =", a * b); //Multiplication
 console.log("a / b =", a / b); //Division
 console.log("a % b =", a % b); //Modulus
 console.log("a ** b =", a ** b); //Exponentiation 10^5=100000
-
-// console.log("a + b =",c);
