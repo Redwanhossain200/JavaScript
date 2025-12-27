6aef990a0d5afcdee9a2e9c5790498e89d803512
learnify <mdredwanhossain2007@gmail.com>
2025-12-27 12:15:48 +0600

    Update file

--- Chapter2/js/operators.js
+++ Chapter2/js/operators.js
@@ -1,24 +1,35 @@
-// Arithmatic Operators
-let a = 10;
-let b = 5;
-let c = a + b;
-// console.log("a + b =",c);
+// // Arithmatic Operators
+// let a = 10;
+// let b = 5;
+// let c = a + b;
+// // console.log("a + b =",c);
 
-console.log("a =", 10, "& b =", 5);
-console.log("a + b =", a + b); //Addition 
-console.log("a - b =", a - b); //Substraction
-console.log("a * b =", a * b); //Multiplication
-console.log("a / b =", a / b); //Division
-console.log("a % b =", a % b); //Modulus
-console.log("a ** b =", a ** b); //Exponentiation 10^5=100000
+// console.log("a =", 10, "& b =", 5);
+// console.log("a + b =", a + b); //Addition 
+// console.log("a - b =", a - b); //Substraction
+// console.log("a * b =", a * b); //Multiplication
+// console.log("a / b =", a / b); //Division
+// console.log("a % b =", a % b); //Modulus
+// console.log("a ** b =", a ** b); //Exponentiation 10^5=100000
 
 
-//Unary Oparators
-let x = 6;
-let y = 4;
-console.log("x =", 6, "& y =", 4);
+// //Unary Oparators
+// let x = 6;
+// let y = 4;
+// console.log("x =", 6, "& y =", 4);
 
-console.log("x++ =", x++); //Post Increment
-console.log("x =", x);
-console.log("++x =", ++x); //Pre Increment
-console.log("x =", x);
+// console.log("x++ =", x++); //Post Increment
+// console.log("x =", x);
+// console.log("++x =", ++x); //Pre Increment
+// console.log("x =", x);
+
+
+// Assignment Operators
+let a = 4;
+let b = 2;
+
+a -= 4; //a = a - 4
+console.log("a = ", a); //8
+
+b += 4; //b = b + 4
+console.log("b = ", b); //8
